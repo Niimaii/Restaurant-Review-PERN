3d5999a6d7045cd58de64c9458abcceed21e42a7
Niimaii <nimaicgomes@gmail.com>
2023-01-23 23:51:20 -0300

    backend and frontend routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
 /* When the front end does a get method towards 
 http://localhost:3001/getRestaurants This will hit the route we established bellow
 */
-app.get("/getRestaurants", (req, res) => {
-  res.json({
+app.get("/api/v1/restaurants", (req, res) => {
+  //This outputs on the website page
+  // Status allows us to send a custom code when this runs
+  res.status(200).json({
     status: "success",
-    restaurant: "mcdonalds",
+    data: {
+      restaurant: ["mcdonalds", "wendy's"],
+    },
   });
 });
 const port = process.env.PORT || 3001;
