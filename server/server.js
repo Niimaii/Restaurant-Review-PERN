90297e3637dad5063da18c5befcdee9ac34c869b
Niimaii <nimaicgomes@gmail.com>
2023-01-25 17:05:32 -0300

    Adding middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
 
 /* When the front end does a get method towards 
 http://localhost:3001/getRestaurants This will hit the route we established bellow
 */
 // Get all restaurants
 app.get("/api/v1/restaurants", (req, res) => {
+  console.log("Route handler ran!");
   //This outputs on the website page
   // Status allows us to send a custom code when this runs
   res.status(200).json({
@@ -32,5 +36,3 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is up and listening on port ${port}`);
 });
-
-console.log("testing");
