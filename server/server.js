ddcd43c60db85109883372098ab4b98fa39577dc
Niimaii <nimaicgomes@gmail.com>
2023-01-26 19:13:15 -0300

    Able to delete from db

--- server/server.js
+++ server/server.js
@@ -99,10 +99,18 @@ app.put("/api/v1/restaurants/:id", async (req, res) => {
 });
 
 // Delete Restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  res.status(204).json({
-    status: "success",
-  });
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = db.query("delete from restaurants where id = $1", [
+      req.params.id,
+    ]);
+
+    res.status(204).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const port = process.env.PORT || 3001;
