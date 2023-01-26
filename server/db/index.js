2701ae433236e7fbfad1b1ff50d7f06d2ef9cdc3
Niimaii <nimaicgomes@gmail.com>
2023-01-26 14:55:12 -0300

    Query setup

---
+++ server/db/index.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+
+// Normally you would have to refrence the .env folder and manually put in the user, host, password, port and so on. However since we already have all of that in the env folder. It does allof that automatically for us
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const db = require("./db");
 const app = express();
 
 // This takes the information from the page (which is in JSON) and attack it to our request under body (i.e req.body). https://youtu.be/J01rYl9T3BU?t=6914
@@ -10,7 +11,10 @@ app.use(express.json());
 http://localhost:3001/getRestaurants This will hit the route we established bellow
 */
 // Get all restaurants
-app.get("/api/v1/restaurants", (req, res) => {
+app.get("/api/v1/restaurants", async (req, res) => {
+  // db.query is a promise
+  const results = await db.query("select * from restaurants");
+  console.log(results);
   console.log("Route handler ran!");
   //This outputs on the website page
   // Status allows us to send a custom code when this runs
