cdb92256772616490a2f894da354b3355735eb05
Niimaii <nimaicgomes@gmail.com>
2023-02-02 23:44:28 -0300

    test

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// https://beta.reactjs.org/reference/react-dom/client/createRoot
+// https://beta.reactjs.org/reference/react-dom/client/createRoot - This is in place ReactDOM
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,7 @@ const { Pool } = require("pg");
 
 // Normally you would have to refrence the .env folder and manually put in the user, host, password, port and so on. However since we already have all of that in the env folder. It does allof that automatically for us
 const pool = new Pool();
+console.log("hey");
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
