fb1b961f03e0125ec3008dbbbbcae16908e29113
Niimaii <nimaicgomes@gmail.com>
2023-02-07 23:50:19 -0300

    form

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
 
 // https://beta.reactjs.org/reference/react-dom/client/createRoot - This is in place ReactDOM
 const domNode = document.getElementById("root");
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import AddRestaurant from "../components/AddRestaurant";
 
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <Header />
+      <AddRestaurant />
     </>
   );
 };
