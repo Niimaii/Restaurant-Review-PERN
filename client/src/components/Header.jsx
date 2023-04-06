fef946c643638f5a2cd782a5cb93630af7daac23
Niimaii <nimaicgomes@gmail.com>
2023-04-06 17:13:59 -0300

    formatting

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
   return (
     <div>
-      <h1 className="font-weight-light display-1text-center">
+      <h1 className='nav-title font-weight-light display-1text-center'>
         Restaurant Finder
       </h1>
     </div>
