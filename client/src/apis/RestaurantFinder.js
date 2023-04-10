ef4df9fb4d08e0ffd193453ca5b4a7c085bf6bd7
Niimaii <nimaicgomes@gmail.com>
2023-04-10 20:34:11 -0300

    Fix deployment

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Change baseURL depending if we are in production or development mode
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'api/v1/restaurants'
+    ? '/api/v1/restaurants'
     : 'http://localhost:4000/api/v1/restaurants';
 
 // Here we are setting the base url for axios. The reason we choose /api/v1/restaurants is because all of our routes have /api/v1/restaurants. So my as well add itas default.
